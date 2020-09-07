53e297359e5b5600c13cc603336493138aaa7742
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-07 03:32:57 +0300

    Header Shaped

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ProductsModule } from './products/products.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProductsModule,
+    CountriesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    id: number;
+    name: string;
+    //inStock?: boolean; // to ? to kanei optional
+    inStock: boolean;
+}
